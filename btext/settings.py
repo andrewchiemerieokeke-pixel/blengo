8a7fc169db4d238092e89887ad8b7f1c020dad37
Andrew6201 <okekechiemerie2021@gmail.com>
2026-02-13 02:56:49 +0200

    Ingrodd pou

--- btext/settings.py
+++ btext/settings.py
@@ -32,7 +32,12 @@ DEBUG = env("DEBUG")
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS") 
 
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://blengos.onrender.com"
+]
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
 # Application definition
 
 INSTALLED_APPS = [
