b23703dbd8687e35b6e2e5e377d0277b7222444b
Andrew6201 <okekechiemerie2021@gmail.com>
2026-03-05 11:55:24 +0200

    added

--- btext/settings.py
+++ btext/settings.py
@@ -19,7 +19,7 @@ environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-SITE_ID = 2
+SITE_ID = 1
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
