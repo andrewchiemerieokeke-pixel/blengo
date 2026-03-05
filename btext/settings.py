31dd76aab843223b6c03db9329b60310c2bdf29c
Andrew6201 <okekechiemerie2021@gmail.com>
2026-03-05 11:11:45 +0200

    added account and

--- btext/settings.py
+++ btext/settings.py
@@ -19,7 +19,7 @@ environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-SITE_ID = 1
+SITE_ID = 2
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -96,6 +96,7 @@ WSGI_APPLICATION = 'btext.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+'''
 
 DATABASES = {
     'default': {
@@ -107,7 +108,7 @@ DATABASES = {
 DATABASES = {
     'default':dj_database_url.parse(env('DATABASE_URL'))
 }
-'''
+
 
 #DATABASES["default"] = dj_database_url.parse()
 
