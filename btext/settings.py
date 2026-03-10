501080a9ca071679a0ce31642a14789fcb27f262
Andrew6201 <okekechiemerie2021@gmail.com>
2026-03-10 15:27:08 +0200

    corrected details template

--- btext/settings.py
+++ btext/settings.py
@@ -96,8 +96,8 @@ WSGI_APPLICATION = 'btext.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-'''
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
